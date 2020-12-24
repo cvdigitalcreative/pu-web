e70235e0136072f0fd08f4fad6ab41d7be39e4d2
zorayaw <zorayaw31@gmail.com>
2020-12-24 23:05:39 +0700

    model functions for Kegiatan_model. havent been tested yet

--- application/models/Kegiatan_model.php
+++ application/models/Kegiatan_model.php
@@ -2,11 +2,13 @@
 
 class Kegiatan_model extends CI_Model
 {
-    public function http_request_get($function)
+    public function http_request_get($function, $token)
     {
+        $dataHeader = ['Authentication: ' . $token];
         $curl = curl_init();
         $url = API_URL . "/kegiatan" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
         $result = curl_exec($curl);
         curl_close($curl);
@@ -14,12 +16,31 @@ class Kegiatan_model extends CI_Model
         return json_decode($result, TRUE);
     }
 
-    public function http_request_post($data, $function)
+    public function http_request_get_tanggal($function, $tanggal, $token)
     {
+        $dataHeader = [
+            'Authentication: ' . $token,
+            'tanggal: ' . $tanggal
+        ];
+        $curl = curl_init();
+        $url = API_URL . "/kegiatan" . $function;
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        return json_decode($result, TRUE);
+    }
+
+    public function http_request_post($data, $function, $token)
+    {
+        $dataHeader = ['Authentication: ' . $token];
         $curl = curl_init();
         $url = API_URL . "/kegiatan" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POST, TRUE);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
         $result = curl_exec($curl);
@@ -28,11 +49,13 @@ class Kegiatan_model extends CI_Model
         return json_decode($result, TRUE);
     }
 
-    public function http_request_update($data, $function)
+    public function http_request_update($data, $function, $token)
     {
+        $dataHeader = ['Authentication: ' . $token];
         $curl = curl_init();
         $url = API_URL . "/kegiatan" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
@@ -42,16 +65,109 @@ class Kegiatan_model extends CI_Model
         return json_decode($result, TRUE);
     }
 
-    public function http_request_delete($function)
+    public function http_request_delete($function, $token)
     {
+        $dataHeader = ['Authentication: ' . $token];
         $curl = curl_init();
         $url = API_URL . "/kegiatan" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
         $result = curl_exec($curl);
         curl_close($curl);
 
         return json_decode($result, TRUE);
+    }
+
+    // ======================= Model Functions ========================
+
+    public function add_kegiatan(
+        $judul_kegiatan,
+        $deskripsi_kegiatan,
+        $tanggal_kegiatan,
+        $lokasi_kegiatan,
+        $latitude_lokasi,
+        $longitude_lokasi,
+        $status_kegiatan,
+        $id_pelatih_kegiatan,
+        $foto_banner_kegiatan,
+        $token
+    ) {
+        $data = [
+            'judul_kegiatan' => $judul_kegiatan,
+            'deskripsi_kegiatan' => $deskripsi_kegiatan,
+            'tanggal_kegiatan' => $tanggal_kegiatan,
+            'lokasi_kegiatan' => $lokasi_kegiatan,
+            'latitude_lokasi' => $latitude_lokasi,
+            'longitude_lokasi' => $longitude_lokasi,
+            'status_kegiatan' => $status_kegiatan,
+            'id_pelatih_kegiatan' => $id_pelatih_kegiatan,
+            'foto_banner_kegiatan' => $foto_banner_kegiatan
+        ];
+
+        return $this->http_request_post($data, "/", $token);
+    }
+
+    public function edit_kegiatan(
+        $judul_kegiatan,
+        $deskripsi_kegiatan,
+        $tanggal_kegiatan,
+        $lokasi_kegiatan,
+        $latitude_lokasi,
+        $longitude_lokasi,
+        $status_kegiatan,
+        $id_pelatih_kegiatan,
+        $foto_banner_kegiatan,
+        $id_kegiatan,
+        $token
+    ) {
+        $data = [
+            'judul_kegiatan' => $judul_kegiatan,
+            'deskripsi_kegiatan' => $deskripsi_kegiatan,
+            'tanggal_kegiatan' => $tanggal_kegiatan,
+            'lokasi_kegiatan' => $lokasi_kegiatan,
+            'latitude_lokasi' => $latitude_lokasi,
+            'longitude_lokasi' => $longitude_lokasi,
+            'status_kegiatan' => $status_kegiatan,
+            'id_pelatih_kegiatan' => $id_pelatih_kegiatan,
+            'foto_banner_kegiatan' => $foto_banner_kegiatan
+        ];
+
+        return $this->http_request_update($data, "/$id_kegiatan", $token);
+    }
+
+    public function delete_kegiatan($id_kegiatan, $token)
+    {
+        return $this->http_request_delete("/$id_kegiatan", $token);
+    }
+
+    public function view_kegiatan($token)
+    {
+        return $this->http_request_get("/", $token);
+    }
+
+    public function view_kegiatan_perbulan($tanggal, $token)
+    {
+        return $this->http_request_get_tanggal("/tanggal/", $tanggal, $token);
+    }
+
+    public function view_detail_kegiatan($id_kegiatan, $token)
+    {
+        return $this->http_request_get("/$id_kegiatan", $token);
+    }
+
+    public function daftar_kegiatan($id_kegiatan, $token)
+    {
+        $data = [
+            'id_kegiatan' => $id_kegiatan
+        ];
+
+        return $this->http_request_post($data, "/daftar/", $token);
+    }
+
+    public function view_peserta_by_status($id_kegiatan, $id_status, $token)
+    {
+        return $this->http_request_get("/$id_kegiatan/$id_status", $token);
     }
 }
