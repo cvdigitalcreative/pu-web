a14247279f296c2b8c3575f080545d1895891257
zorayaw <zorayaw31@gmail.com>
2020-12-25 00:37:28 +0700

    Model function for SKA_model. havent been tested yet

--- application/models/SKA_model.php
+++ application/models/SKA_model.php
@@ -2,11 +2,13 @@
 
 class SKA_model extends CI_Model
 {
-    public function http_request_get($function)
+    public function http_request_get($function, $token)
     {
+        $dataHeader = ['Authentication: ' . $token];
         $curl = curl_init();
         $url = API_URL . "/ska" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
         $result = curl_exec($curl);
         curl_close($curl);
@@ -14,12 +16,14 @@ class SKA_model extends CI_Model
         return json_decode($result, TRUE);
     }
 
-    public function http_request_post($data, $function)
+    public function http_request_post($data, $function, $token)
     {
+        $dataHeader = ['Authentication: ' . $token];
         $curl = curl_init();
         $url = API_URL . "/ska" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_POST, TRUE);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
         $result = curl_exec($curl);
@@ -28,11 +32,13 @@ class SKA_model extends CI_Model
         return json_decode($result, TRUE);
     }
 
-    public function http_request_update($data, $function)
+    public function http_request_update($data, $function, $token)
     {
+        $dataHeader = ['Authentication: ' . $token];
         $curl = curl_init();
         $url = API_URL . "/ska" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
@@ -42,16 +48,70 @@ class SKA_model extends CI_Model
         return json_decode($result, TRUE);
     }
 
-    public function http_request_delete($function)
+    public function http_request_delete($function, $token)
     {
+        $dataHeader = ['Authentication: ' . $token];
         $curl = curl_init();
         $url = API_URL . "/ska" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
         $result = curl_exec($curl);
         curl_close($curl);
 
         return json_decode($result, TRUE);
+    }
+
+    // ======================= Model Functions ========================
+
+    public function add_SKA(
+        $judul_ska,
+        $deskripsi_ska,
+        $id_kategori_ska,
+        $file_ska,
+        $token
+    ) {
+        $data = [
+            'judul_ska' => $judul_ska,
+            'deskripsi_ska' => $deskripsi_ska,
+            'id_kategori_ska' => $id_kategori_ska,
+            'file_ska' => $file_ska,
+        ];
+
+        return $this->http_request_post($data, "/", $token);
+    }
+
+    public function edit_SKA(
+        $judul_ska,
+        $deskripsi_ska,
+        $id_kategori_ska,
+        $file_ska,
+        $id_ska,
+        $token
+    ) {
+        $data = [
+            'judul_ska' => $judul_ska,
+            'deskripsi_ska' => $deskripsi_ska,
+            'id_kategori_ska' => $id_kategori_ska,
+            'file_ska' => $file_ska,
+        ];
+
+        return $this->http_request_update($data, "/$id_ska", $token);
+    }
+
+    public function delete_SKA($id_ska, $token)
+    {
+        return $this->http_request_delete("/$id_ska", $token);
+    }
+
+    public function view_SKA($token)
+    {
+        return $this->http_request_get("/", $token);
+    }
+
+    public function view_SKA_detail($id_ska, $token)
+    {
+        return $this->http_request_get("/$id_ska", $token);
     }
 }
