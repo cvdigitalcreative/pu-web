cf15bac4c2fe4e5ae649dfe3837433cfb937260f
zorayaw <zorayaw31@gmail.com>
2020-12-25 02:08:09 +0700

    function for peserta_kegiatan_model. Havent been tested yet

--- application/models/Peserta_Kegiatan_model.php
+++ application/models/Peserta_Kegiatan_model.php
@@ -2,37 +2,14 @@
 
 class Peserta_Kegiatan_model extends CI_Model
 {
-    public function http_request_get($function)
+
+    public function http_request_update($data, $function, $token)
     {
+        $dataHeader = ['Authentication: ' . $token];
         $curl = curl_init();
         $url = API_URL . "/peserta-kegiatan" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        $result = curl_exec($curl);
-        curl_close($curl);
-
-        return json_decode($result, TRUE);
-    }
-
-    public function http_request_post($data, $function)
-    {
-        $curl = curl_init();
-        $url = API_URL . "/peserta-kegiatan" . $function;
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_POST, TRUE);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        $result = curl_exec($curl);
-        curl_close($curl);
-
-        return json_decode($result, TRUE);
-    }
-
-    public function http_request_update($data, $function)
-    {
-        $curl = curl_init();
-        $url = API_URL . "/peserta-kegiatan" . $function;
-        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
         curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
@@ -42,16 +19,19 @@ class Peserta_Kegiatan_model extends CI_Model
         return json_decode($result, TRUE);
     }
 
-    public function http_request_delete($function)
-    {
-        $curl = curl_init();
-        $url = API_URL . "/peserta-kegiatan" . $function;
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        $result = curl_exec($curl);
-        curl_close($curl);
+    // ======================= Model Functions ========================
 
-        return json_decode($result, TRUE);
+    public function update_peserta_kegiatan(
+        $id_peserta,
+        $id_status_peserta_kegiatan,
+        $id_jadwal_kegiatan,
+        $token
+    ) {
+        $data = [
+            'id_peserta' => $id_peserta,
+            'id_status_peserta_kegiatan' => $id_status_peserta_kegiatan
+        ];
+
+        $this->http_request_update($data, "/$id_jadwal_kegiatan", $token);
     }
 }
