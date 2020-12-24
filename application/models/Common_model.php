31f2e91bc5cfc6b19d6883b1f9cd634fb0e95759
zorayaw <zorayaw31@gmail.com>
2020-12-25 01:11:33 +0700

    Model function for common_model. havent been tested yet

--- application/models/Common_model.php
+++ application/models/Common_model.php
@@ -2,11 +2,13 @@
 
 class Common_model extends CI_Model
 {
-    public function http_request_get($function)
+    public function http_request_get($function, $token)
     {
+        $dataHeader = ['Authentication: ' . $token];
         $curl = curl_init();
         $url = API_URL . "/common" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
         $result = curl_exec($curl);
         curl_close($curl);
@@ -14,44 +16,70 @@ class Common_model extends CI_Model
         return json_decode($result, TRUE);
     }
 
-    public function http_request_post($data, $function)
-    {
-        $curl = curl_init();
-        $url = API_URL . "/common" . $function;
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_POST, TRUE);
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        $result = curl_exec($curl);
-        curl_close($curl);
+    // ======================= Model Functions ========================
 
-        return json_decode($result, TRUE);
+    public function view_jenis_kelamin($token)
+    {
+        return $this->http_request_get("/jenis-kelamin", $token);
     }
 
-    public function http_request_update($data, $function)
+    public function view_status_perkawinan($token)
     {
-        $curl = curl_init();
-        $url = API_URL . "/common" . $function;
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
-        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        $result = curl_exec($curl);
-        curl_close($curl);
-
-        return json_decode($result, TRUE);
+        return $this->http_request_get("/status-perkawinan", $token);
     }
 
-    public function http_request_delete($function)
+    public function view_status_rumah($token)
     {
-        $curl = curl_init();
-        $url = API_URL . "/common" . $function;
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-        $result = curl_exec($curl);
-        curl_close($curl);
+        return $this->http_request_get("/status-rumah", $token);
+    }
 
-        return json_decode($result, TRUE);
+    public function view_pendidikan($token)
+    {
+        return $this->http_request_get("/pendidikan", $token);
+    }
+
+    public function view_jabker($token)
+    {
+        return $this->http_request_get("/jabker", $token);
+    }
+
+    public function view_provinsi($token)
+    {
+        return $this->http_request_get("/provinsi", $token);
+    }
+    
+    public function view_kabupaten_kota($id_provinsi, $token)
+    {
+        return $this->http_request_get("/kabupaten-kota/$id_provinsi", $token);
+    }
+
+    public function view_kecamatan($id_kabupaten_kota, $token)
+    {
+        return $this->http_request_get("/kecamatan/$id_kabupaten_kota", $token);
+    }
+
+    public function view_kelurahan_desa($token)
+    {
+        return $this->http_request_get("/kelurahan-desa", $token);
+    }
+
+    public function view_skkni_ska($token)
+    {
+        return $this->http_request_get("/sskni-ska", $token);
+    }
+
+    public function view_kategori_modul($token)
+    {
+        return $this->http_request_get("/modul", $token);
+    }
+
+    public function view_kategori_tenaga_ahli($token)
+    {
+        return $this->http_request_get("/kategori-tenaga-ahli", $token);
+    }
+
+    public function view_status_peserta($token)
+    {
+        return $this->http_request_get("/status-peserta-kegiatan", $token);
     }
 }
