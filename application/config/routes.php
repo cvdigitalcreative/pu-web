df46048e5f331959d5229386ccc3d98e039a0416
zorayaw <zorayaw31@gmail.com>
2020-12-27 16:39:52 +0700

    adding routes to load tenaga ahli index

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['Tenaga_ahli/(:num)'] = 'Tenaga_ahli/index/$1';
