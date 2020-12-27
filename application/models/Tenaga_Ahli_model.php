a4443941955e688326d2c25ea035d4b1f9ac2688
zorayaw <zorayaw31@gmail.com>
2020-12-27 16:41:15 +0700

    model fixing, add function delete jabker

--- application/models/Tenaga_Ahli_model.php
+++ application/models/Tenaga_Ahli_model.php
@@ -22,8 +22,8 @@ class Tenaga_Ahli_model extends CI_Model
         $curl = curl_init();
         $url = API_URL . "/tenaga-ahli" . $function;
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_POST, TRUE);
         curl_setopt($curl, CURLOPT_HTTPHEADER, $dataHeader);
+        curl_setopt($curl, CURLOPT_POST, TRUE);
         curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
         $result = curl_exec($curl);
@@ -91,13 +91,11 @@ class Tenaga_Ahli_model extends CI_Model
             'alamat_rumah' => $alamat_rumah,
             'id_provinsi' => $id_provinsi,
             'id_kabupaten_kota' => $id_kabupaten_kota,
-            'no_telepon_rumah' => $no_telepon_rumah,
+            'no_telpon_rumah' => $no_telepon_rumah,
             'no_handphone' => $no_handphone,
             'id_jabker' => $id_jabker,
             'id_kategori_tenaga_ahli' => $id_kategori_tenaga_ahli,
         ];
-
-        $data = http_build_query($data);
 
         return $this->http_request_post($data, "/", $token);
     }
@@ -129,15 +127,16 @@ class Tenaga_Ahli_model extends CI_Model
             'alamat_rumah' => $alamat_rumah,
             'id_provinsi' => $id_provinsi,
             'id_kabupaten_kota' => $id_kabupaten_kota,
-            'no_telepon_rumah' => $no_telepon_rumah,
+            'no_telpon_rumah' => $no_telepon_rumah,
             'no_handphone' => $no_handphone,
             'id_jabker' => $id_jabker,
             'id_kategori_tenaga_ahli' => $id_kategori_tenaga_ahli,
         ];
 
+
         $data = http_build_query($data);
 
-        return $this->http_request_post($data, "/$id_tenaga_ahli", $token);
+        return $this->http_request_update($data, "/$id_tenaga_ahli", $token);
     }
 
     public function delete_tenaga_ahli($id_tenaga_ahli, $token)
@@ -150,13 +149,18 @@ class Tenaga_Ahli_model extends CI_Model
         return $this->http_request_get("/$id_kategori_tenaga_ahli", $token);
     }
 
-    public function view_tenaga_ahli_by_provinsi($id_provinsi, $token)
+    public function view_tenaga_ahli_by_provinsi($id_provinsi, $id_kategori_tenaga_ahli, $token)
     {
-        return $this->http_request_get("/provinsi/$id_provinsi", $token);
+        return $this->http_request_get("/provinsi/$id_provinsi?id_kategori_tenaga_ahli=$id_kategori_tenaga_ahli", $token);
     }
 
     public function view_detail_tenaga_ahli($id_tenaga_ahli, $token)
     {
         return $this->http_request_get("/$id_tenaga_ahli", $token);
+    }
+
+    public function delete_jabatan_kerja($id_jabker, $token)
+    {
+        return $this->http_request_get("/jabatan-kerja-ahli/$id_jabker", $token);
     }
 }
