872ca429cd4316f5f9c2de84a65d398bc0827d24
zorayaw <zorayaw31@gmail.com>
2020-12-28 13:31:58 +0700

    controller SKA

---
+++ application/controllers/SKA.php
@@ -0,0 +1,141 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class SKA extends CI_Controller
+{
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('SKA_model');
+    }
+
+    public function index()
+    {
+        if ($this->session->userdata('logged_in') == true) {
+            $null = false;
+            $data['ska'] = $this->SKA_model->view_ska($this->session->userdata('token'));
+            if ($data['ska'] == null)
+                $null = true;
+            else {
+                if ($data['ska']['status'] == "Success") {
+                    $data['ska'] = $data['ska']['data'];
+                } else {
+                    $data['ska'] = null;
+                    $this->session->set_flashdata('APImessage', $data['ska']['message']);
+                }
+            }
+
+            if ($null)
+                redirect();
+        } else
+            redirect();
+    }
+
+    public function detail($id_ska)
+    {
+        if ($this->session->userdata('logged_in') == true) {
+            $null = false;
+            $data['ska'] = $this->SKA_model->view_ska_detail($id_ska, $this->session->userdata('token'));
+            if ($data['ska'] == null)
+                $null = true;
+            else {
+                if ($data['ska']['status'] == "Success") {
+                    $data['ska'] = $data['ska']['data'];
+                } else {
+                    $data['ska'] = null;
+                    $this->session->set_flashdata('APImessage', $data['ska']['message']);
+                }
+            }
+
+            if ($null)
+                redirect();
+        } else
+            redirect();
+    }
+
+    public function tambah_ska_action()
+    {
+        if ($this->session->userdata('logged_in') == true) {
+            $judul_ska = $this->input->post('judul_ska');
+            $deskripsi_ska = $this->input->post('deskripsi_ska');
+            $id_kategori_ska = $this->input->post('id_kategori_ska');
+            $file_ska = new \CurlFile($_FILES['file_ska']['tmp_name'], $_FILES['file_ska']['type'], $_FILES['file_ska']['name']);
+
+            $tambah_ska = $this->SKA_model->add_ska(
+                $judul_ska,
+                $deskripsi_ska,
+                $id_kategori_ska,
+                $file_ska,
+                $this->session->userdata('token')
+            );
+
+            if ($tambah_ska == null) {
+                redirect();
+            }
+            if ($tambah_ska['status'] == "Success") {
+                $this->session->set_flashdata('success', $tambah_ska['message']);
+                redirect();
+            } else {
+                $this->session->set_flashdata('APImessage', $tambah_ska['message']);
+                redirect();
+            }
+        } else {
+            redirect();
+        }
+    }
+
+    //blm done
+    public function edit_ska_action($id_ska)
+    {
+        if ($this->session->userdata('logged_in') == true) {
+            $judul_ska = $this->input->post('judul_ska');
+            $deskripsi_ska = $this->input->post('deskripsi_ska');
+            $id_kategori_ska = $this->input->post('id_kategori_ska');
+            $file_ska = new \CurlFile($_FILES['file_ska']['tmp_name'], $_FILES['file_ska']['type'], $_FILES['file_ska']['name']);
+
+            $edit_ska = $this->SKA_model->edit_ska(
+                $judul_ska,
+                $deskripsi_ska,
+                $id_kategori_ska,
+                $file_ska,
+                $id_ska,
+                $this->session->userdata('token')
+            );
+
+            var_dump($edit_ska); die;
+            if ($edit_ska == null) {
+                redirect();
+            }
+            if ($edit_ska['status'] == "Success") {
+                $this->session->set_flashdata('success', $edit_ska['message']);
+                redirect();
+            } else {
+                $this->session->set_flashdata('APImessage', $edit_ska['message']);
+                redirect();
+            }
+        } else {
+            redirect();
+        }
+    }
+
+    public function delete_ska($id_ska)
+    {
+        if ($this->session->userdata('logged_in') == true) {
+            $delete_ska = $this->SKA_model->delete_ska($id_ska, $this->session->userdata('token'));
+            if ($delete_ska == null) {
+                redirect();
+            } else {
+                if ($delete_ska['status'] == "Success") {
+                    $this->session->set_flashdata('success', $delete_ska['message']);
+                    redirect();
+                } else {
+                    $this->session->set_flashdata('APImessage', $delete_ska['message']);
+                    redirect();
+                }
+            }
+        } else
+            redirect();
+    }
+}
