32cd4838a980e281d91c3424f66c85ac944dbbf1
Mdedealf <dede.alfaruqq@gmail.com>
2021-01-03 13:41:29 +0700

    Dede - adding illustration, and edit some css

--- application/views/components/js.php
+++ application/views/components/js.php
@@ -11,10 +11,6 @@
 <!-- Page level plugins -->
 <!-- <script src="<?=base_url('assets/chart.js/Chart.min.js')?>"></script>   -->
 
-<!-- Page level custom scripts -->
-<!-- <script src="<?=base_url('assets/js/demo/chart-area-demo.js')?>"></script> -->
-<!-- <script src="<?=base_url('assets/js/demo/chart-pie-demo.js')?>"></script> -->
-
 <!-- Page level plugins -->
 <script src="<?=base_url('assets/datatables/jquery.dataTables.min.js')?>"></script>
 <script src="<?=base_url('assets/datatables/dataTables.bootstrap4.min.js')?>"></script>
--- application/views/components/sidebar.php
+++ application/views/components/sidebar.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
 	<!-- Sidebar - Brand -->
-	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url('admin/dashboard')?>">
+	<a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?=base_url('pupr/dashboard')?>">
 		<div class="sidebar-brand-icon">
 			<img src="<?=base_url('assets/image/pupr-logo.svg')?>" width="34" height="34" alt="BJKW II Palembang">
 		</div>
@@ -15,8 +15,8 @@
 	<!-- Nav Item - Dashboard -->
 	<li class="nav-item <?=$this->uri->segment(2) == 'dashboard' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'dashboard' ? 'disabled' : ''?>"
-			href="<?=base_url('admin/dashboard')?>">
-			<i class="fas fa-fw fa-tachometer-alt"></i>
+			href="<?=base_url('pupr/dashboard')?>">
+			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/dashboard-menu.svg')?>">
 			<span>Dashboard</span>
 		</a>
 	</li>
@@ -25,7 +25,7 @@
 	<li class="nav-item <?=$this->uri->segment(2) == 'confirmPayment' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'confirmPayment' ? 'disabled' : ''?>"
 			href="<?=base_url('admin/confirmPayment')?>">
-			<i class="fas fa-fw fa fa-check-circle"></i>
+			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/kegiatan-menu.svg')?>">
 			<span>Kalender Kegiatan</span>
 		</a>
 	</li>
@@ -34,7 +34,7 @@
 	<li class="nav-item <?=$this->uri->segment(2) == 'confirmWithdraw' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'confirmWithdraw' ? 'disabled' : ''?>"
 			href="<?=base_url('admin/confirmWithdraw')?>">
-			<i class="fas fa-fw fa fa-check-circle"></i>
+			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/ahli-menu.svg')?>">
 			<span>Tenaga Ahli & Mitra</span>
 		</a>
 	</li>
@@ -43,7 +43,7 @@
 	<li class="nav-item <?=$this->uri->segment(2) == 'confirmDriver' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'confirmDriver' ? 'disabled' : ''?>"
 			href="<?=base_url('admin/confirmDriver')?>">
-			<i class="fas fa-fw fa fa-check-circle"></i>
+			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/peserta-menu.svg')?>">
 			<span>Peserta</span>
 		</a>
 	</li>
@@ -52,7 +52,7 @@
 	<li class="nav-item <?=$this->uri->segment(2) == 'emergency' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'emergency' ? 'disabled' : ''?>"
 			href="<?=base_url('admin/emergency')?>">
-			<i class="fas fa-fw fa fa-exclamation-triangle"></i>
+			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/buku-saku-menu.svg')?>">
 			<span>Buku Saku</span>
 		</a>
 	</li>
@@ -61,7 +61,7 @@
 	<li class="nav-item <?=$this->uri->segment(2) == 'help' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'help' ? 'disabled' : ''?>"
 			href="<?=base_url('admin/help')?>">
-			<i class="fas fa-fw fa fa-question-circle"></i>
+			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/administrasi-menu.svg')?>">
 			<span>Administrasi Kegiatan</span>
 		</a>
 	</li>
@@ -70,7 +70,7 @@
 	<li class="nav-item <?=$this->uri->segment(2) == 'skkni' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'skkni' ? 'disabled' : ''?>"
 			href="<?=base_url('admin/skkni')?>">
-			<i class="fas fa-fw fa fa-question-circle"></i>
+			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/surat-menu.svg')?>">
 			<span>SKKNI</span>
 		</a>
 	</li>
@@ -79,7 +79,7 @@
 	<li class="nav-item <?=$this->uri->segment(2) == 'modul' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'modul' ? 'disabled' : ''?>"
 			href="<?=base_url('admin/modul')?>">
-			<i class="fas fa-fw fa fa-question-circle"></i>
+			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/modul-menu.svg')?>">
 			<span>Modul</span>
 		</a>
 	</li>
