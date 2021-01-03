06aa4cb9d3a24e82be04602f10972142c430393a
Mdedealf <dede.alfaruqq@gmail.com>
2021-01-03 17:04:31 +0700

    Dede - adding new login view

--- application/views/components/sidebar.php
+++ application/views/components/sidebar.php
@@ -6,7 +6,7 @@
 		<div class="sidebar-brand-icon">
 			<img src="<?=base_url('assets/image/pupr-logo.svg')?>" width="34" height="34" alt="BJKW II Palembang">
 		</div>
-		<div class="sidebar-brand-text mx-3">BJKW II Palembang</sup></div>
+		<div class="sidebar-brand-text mx-3">BJKW II Palembang</div>
 	</a>
 
 	<!-- Divider -->
@@ -24,7 +24,7 @@
 	<!-- Nav Item - Kalender Kegiatan -->
 	<li class="nav-item <?=$this->uri->segment(2) == 'confirmPayment' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'confirmPayment' ? 'disabled' : ''?>"
-			href="<?=base_url('admin/confirmPayment')?>">
+			href="<?=base_url('pupr/events')?>">
 			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/kegiatan-menu.svg')?>">
 			<span>Kalender Kegiatan</span>
 		</a>
@@ -33,7 +33,7 @@
 	<!-- Nav Item - Tenaga Ahli & Mitra -->
 	<li class="nav-item <?=$this->uri->segment(2) == 'confirmWithdraw' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'confirmWithdraw' ? 'disabled' : ''?>"
-			href="<?=base_url('admin/confirmWithdraw')?>">
+			href="<?=base_url('pupr/experts/')?>">
 			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/ahli-menu.svg')?>">
 			<span>Tenaga Ahli & Mitra</span>
 		</a>
@@ -42,7 +42,7 @@
 	<!-- Nav Item - Peserta -->
 	<li class="nav-item <?=$this->uri->segment(2) == 'confirmDriver' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'confirmDriver' ? 'disabled' : ''?>"
-			href="<?=base_url('admin/confirmDriver')?>">
+			href="<?=base_url('pupr/participant')?>">
 			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/peserta-menu.svg')?>">
 			<span>Peserta</span>
 		</a>
@@ -51,7 +51,7 @@
 	<!-- Nav Item - Buku Saku -->
 	<li class="nav-item <?=$this->uri->segment(2) == 'emergency' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'emergency' ? 'disabled' : ''?>"
-			href="<?=base_url('admin/emergency')?>">
+			href="<?=base_url('pupr/pocketbook')?>">
 			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/buku-saku-menu.svg')?>">
 			<span>Buku Saku</span>
 		</a>
@@ -60,7 +60,7 @@
 	<!-- Nav Item - Administrasi Kegiatan -->
 	<li class="nav-item <?=$this->uri->segment(2) == 'help' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'help' ? 'disabled' : ''?>"
-			href="<?=base_url('admin/help')?>">
+			href="<?=base_url('pupr/administration')?>">
 			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/administrasi-menu.svg')?>">
 			<span>Administrasi Kegiatan</span>
 		</a>
@@ -69,7 +69,7 @@
 	<!-- Nav Item - SKKNI -->
 	<li class="nav-item <?=$this->uri->segment(2) == 'skkni' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'skkni' ? 'disabled' : ''?>"
-			href="<?=base_url('admin/skkni')?>">
+			href="<?=base_url('pupr/skkni')?>">
 			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/surat-menu.svg')?>">
 			<span>SKKNI</span>
 		</a>
@@ -78,7 +78,7 @@
 	<!-- Nav Item - Modul -->
 	<li class="nav-item <?=$this->uri->segment(2) == 'modul' ? 'active' : ''?>">
 		<a class="nav-link <?=$this->uri->segment(2) == 'modul' ? 'disabled' : ''?>"
-			href="<?=base_url('admin/modul')?>">
+			href="<?=base_url('pupr/modul')?>">
 			<img class="img-profile mr-2" src="<?=base_url('assets/illustration/modul-menu.svg')?>">
 			<span>Modul</span>
 		</a>
