d741acac32080beecfdfa9c665e2a87d67676cc5
zorayaw <zorayaw31@gmail.com>
2021-01-06 22:01:45 +0700

    fixing data for kegiatan, data input for tambah and edit kegiatan, function view kota for form

--- application/controllers/Kegiatan.php
+++ application/controllers/Kegiatan.php
@@ -40,6 +40,60 @@ class Kegiatan extends CI_Controller
 
                     $indexKegiatan = 0;
                     foreach ($data['kegiatan'] as $val) {
+
+                        // ======================= tanggal Kegiatan ================================
+                        $tanggal_kegiatan = $val['tanggal_kegiatan'];
+                        $temparr = explode('-', $tanggal_kegiatan);
+                        $indextglreverse = 0;
+                        for ($j = count($temparr) - 1; $j >= 0; $j--) {
+                            $arrtemptanggal[$indextglreverse] = $temparr[$j];
+                            $indextglreverse++;
+                        }
+                        $data['kegiatan'][$indexKegiatan]['tanggal_kegiatan'] = implode('-', $arrtemptanggal);
+                        $data['kegiatan'][$indexKegiatan]['tanggal_kegiatan_text'] = strtotime($val['tanggal_kegiatan']);
+                        $data['kegiatan'][$indexKegiatan]['tanggal_kegiatan_text'] = date("d F Y", $data['kegiatan'][$indexKegiatan]['tanggal_kegiatan_text']);
+
+                        // ======================= tanggal Kegiatan Selesai ================================
+                        $tanggal_kegiatan_selesai = $val['tanggal_kegiatan_selesai'];
+                        $temparrselesai = explode('-', $tanggal_kegiatan_selesai);
+                        $indextglselesaireverse = 0;
+                        for ($j = count($temparrselesai) - 1; $j >= 0; $j--) {
+                            $arrtemptanggalselesai[$indextglselesaireverse] = $temparrselesai[$j];
+                            $indextglselesaireverse++;
+                        }
+                        $data['kegiatan'][$indexKegiatan]['tanggal_kegiatan_selesai'] = implode('-', $arrtemptanggalselesai);
+                        $data['kegiatan'][$indexKegiatan]['tanggal_kegiatan_selesai_text'] = strtotime($val['tanggal_kegiatan_selesai']);
+                        $data['kegiatan'][$indexKegiatan]['tanggal_kegiatan_selesai_text'] = date("d F Y", $data['kegiatan'][$indexKegiatan]['tanggal_kegiatan_selesai_text']);
+
+                        // ==================== Instruktur Kegiatan ===========================
+                        $indexInstruktur = 0;
+                        foreach ($data['kegiatan'][$indexKegiatan]['instruktur_kegiatan'] as $val2) {
+                            if ($indexInstruktur == 0) {
+                                $data['kegiatan'][$indexKegiatan]['str_nama_instruktur_kegiatan'] = $val2['nama'];
+                            } else
+                                $data['kegiatan'][$indexKegiatan]['str_nama_instruktur_kegiatan'] = (string)  $data['kegiatan'][$indexKegiatan]['str_nama_instruktur_kegiatan'] . ", " . $val2['nama'];
+                            $indexInstruktur++;
+                        }
+
+                        // ==================== Asesor Kegiatan ===========================
+                        $indexAsesor = 0;
+                        foreach ($data['kegiatan'][$indexKegiatan]['asesor_kegiatan'] as $val2) {
+                            if ($indexAsesor == 0) {
+                                $data['kegiatan'][$indexKegiatan]['str_nama_asesor_kegiatan'] = $val2['nama'];
+                            } else
+                                $data['kegiatan'][$indexKegiatan]['str_nama_asesor_kegiatan'] = (string)  $data['kegiatan'][$indexKegiatan]['asesor_kegiatan']['str_nama_asesor_kegiatan'] . ", " . $val2['nama'];
+                            $indexAsesor++;
+                        }
+                        // ==================== pelatih Kegiatan ===========================
+                        $indexPelatih = 0;
+                        foreach ($data['kegiatan'][$indexKegiatan]['pelatih_kegiatan'] as $val2) {
+                            if ($indexPelatih == 0) {
+                                $data['kegiatan'][$indexKegiatan]['str_nama_pelatih_kegiatan'] = $val2['nama'];
+                            } else
+                                $data['kegiatan'][$indexKegiatan]['str_nama_pelatih_kegiatan'] = (string)  $data['kegiatan'][$indexKegiatan]['str_nama_pelatih_kegiatan'] . ", " . $val2['nama'];
+                            $indexPelatih++;
+                        }
+
                         $data['kegiatan'][$indexKegiatan]['berita_acara'] = $this->Kegiatan_model->view_berita_acara($val['id_kegiatan'], $this->session->userdata('token'));
                         if ($data['kegiatan'][$indexKegiatan]['berita_acara'] == null)
                             $null = true;
@@ -98,6 +152,20 @@ class Kegiatan extends CI_Controller
             }
 
             $data['jumlah_kegiatan_berjalan'] = $data['jumlah_seluruh_kegiatan'] - $data['jumlah_kegiatan_selesai'];
+
+
+            // ============================== data for form =================================
+            $data['provinsi'] = $this->Common_model->view_provinsi($this->session->userdata('token'));
+            if ($data['provinsi'] == null)
+                $null = true;
+            else {
+                if ($data['provinsi']['status'] == "Success") {
+                    $data['provinsi'] = $data['provinsi']['data'];
+                } else {
+                    $data['provinsi'] = null;
+                    $this->session->set_flashdata('APImessage', $data['provinsi']['message']);
+                }
+            }
 
             if ($null)
                 redirect("pupr/dashboard");
@@ -580,6 +648,25 @@ class Kegiatan extends CI_Controller
             redirect("pupr/login");
     }
 
+    public function view_kota()
+    {
+        $provinsi = $this->input->post('id_provinsi');
+        $data = $this->Common_model->view_kabupaten_kota($provinsi, $this->session->userdata('token'));
+        if ($data == null) {
+            $this->load->view('error_page');
+        } else {
+            if ($data['status'] == "Success") {
+                $data = $data['data'];
+                foreach ($data as $row) {
+                    echo '<option value="' . $row['id_kabupaten_kota'] . '">' . $row['kabupaten_kota'] . '</option>';
+                }
+            } else {
+                $data = null;
+                echo '<option value="' . '">Gagal mendapatkan kota untuk kabupaten yang dipilih</option>';
+            }
+        }
+    }
+
     public function tambah_kegiatan_action()
     {
         if ($this->session->userdata('logged_in') == true) {
@@ -611,9 +698,9 @@ class Kegiatan extends CI_Controller
             $id_jenis_kegiatan = $this->input->post('id_jenis_kegiatan');
             $id_provinsi = $this->input->post('id_provinsi');
             $id_kota_kabupaten = $this->input->post('id_kota_kabupaten');
-            $id_pelatih_kegiatan = $this->input->post('id_pelatih_kegiatan');
             $id_asesor_kegiatan = $this->input->post('id_asesor_kegiatan');
             $id_instruktur_kegiatan = $this->input->post('id_instruktur_kegiatan');
+            $file_materi_kegiatan = new \CurlFile($_FILES['file_materi_kegiatan']['tmp_name'], $_FILES['file_materi_kegiatan']['type'], $_FILES['file_materi_kegiatan']['name']);
 
             $tambah_kegiatan = $this->Kegiatan_model->add_kegiatan(
                 $judul_kegiatan,
@@ -629,9 +716,9 @@ class Kegiatan extends CI_Controller
                 $id_jenis_kegiatan,
                 $id_provinsi,
                 $id_kota_kabupaten,
-                $id_pelatih_kegiatan,
                 $id_asesor_kegiatan,
                 $id_instruktur_kegiatan,
+                $file_materi_kegiatan,
                 $this->session->userdata('token')
             );
 
@@ -809,9 +896,9 @@ class Kegiatan extends CI_Controller
             $id_jenis_kegiatan = $this->input->post('id_jenis_kegiatan');
             $id_provinsi = $this->input->post('id_provinsi');
             $id_kota_kabupaten = $this->input->post('id_kota_kabupaten');
-            $id_pelatih_kegiatan = $this->input->post('id_pelatih_kegiatan');
             $id_asesor_kegiatan = $this->input->post('id_asesor_kegiatan');
             $id_instruktur_kegiatan = $this->input->post('id_instruktur_kegiatan');
+            $file_materi_kegiatan = new \CurlFile($_FILES['file_materi_kegiatan']['tmp_name'], $_FILES['file_materi_kegiatan']['type'], $_FILES['file_materi_kegiatan']['name']);
 
             $id_provinsi = 0;
             $id_kota_kabupaten = 0;
@@ -829,9 +916,9 @@ class Kegiatan extends CI_Controller
                 $id_jenis_kegiatan,
                 $id_provinsi,
                 $id_kota_kabupaten,
-                $id_pelatih_kegiatan,
                 $id_asesor_kegiatan,
                 $id_instruktur_kegiatan,
+                $file_materi_kegiatan,
                 $id_kegiatan,
                 $this->session->userdata('token')
             );
