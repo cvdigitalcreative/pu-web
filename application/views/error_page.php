895667bf280c012715071199b528918bad72eff7
zorayaw <zorayaw31@gmail.com>
2021-01-06 19:47:52 +0700

    error page view & css

---
+++ application/views/error_page.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php $this->load->view("components/head.php") ?>
+    <link href="<?php echo base_url('assets/css/error.css') ?>" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="pt-5 pl-5">
+        <span class="masuk-title">Maaf atas ketidaknyamanan Anda.</span>
+        <h6 class="masuk-title">Kami tidak dapat tersambung ke server saat ini.</h6>
+            <p class="mt-2 keterangan-title mb-2 mt-3">Mohon muat ulang halaman beberapa saat lagi. Terima kasih.
+    </div>
+
+    <?php $this->load->view("components/js.php") ?>
+</body>
+
+</html>
