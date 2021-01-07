488b7c5b067505d6fa01b05ea600b67e69eb6b10
Mdedealf <dede.alfaruqq@gmail.com>
2021-01-07 16:40:32 +0700

    beautify format in events.php

--- application/views/administrator/events.php
+++ application/views/administrator/events.php
@@ -18,15 +18,16 @@
 					</button>
 				</div>
 				<div class="modal-body">
-					<form method="POST" action="<?= base_url()?>Kegiatan/tambah_kegiatan_action" enctype="multipart/form-data">
+					<form method="POST" action="<?= base_url()?>Kegiatan/tambah_kegiatan_action"
+						enctype="multipart/form-data">
 						<div class="form-group py-2">
 							<label for="akunKegiatan">Akun Kegiatan*</label>
 							<select class="form-control" id="akun-kegiatan" name="id_akun_kegiatan" required>
 								<option selected disabled>Pilih akun kegiatan</option>
 								<?php if($akun_kegiatan != null):
 								foreach ($akun_kegiatan as $row):?>
-									<option value="<?=$row['id_akun_kegiatan']?>"><?= $row['akun_kegiatan']?></option>
-							<?php endforeach; endif?>
+								<option value="<?=$row['id_akun_kegiatan']?>"><?= $row['akun_kegiatan']?></option>
+								<?php endforeach; endif?>
 							</select>
 						</div>
 						<div class="form-group py-2">
@@ -35,30 +36,36 @@
 								<option selected disabled>Pilih jenis kegiatan</option>
 								<?php if($jenis_kegiatan != null):
 								foreach ($jenis_kegiatan as $row):?>
-									<option value="<?=$row['id_jenis_kegiatan']?>"><?= $row['jenis_kegiatan']?></option>
-							<?php endforeach; endif?>
+								<option value="<?=$row['id_jenis_kegiatan']?>"><?= $row['jenis_kegiatan']?></option>
+								<?php endforeach; endif?>
 							</select>
 						</div>
 						<div class="form-group py-2">
 							<label for="bannerKegiatan">Banner Kegiatan *</label>
 							<div id="profile-container">
-								<image id="banner-image" src="<?= base_url('assets/icons/pupr-add-image-icon.svg') ?>" />
-								<input id="banner-kegiatan" type="file" name="foto_banner_kegiatan" id="bannerKegiatan" placeholder="Photo" alt="Pilih Foto" required="" capture>
+								<image id="banner-image"
+									src="<?= base_url('assets/icons/pupr-add-image-icon.svg') ?>" />
+								<input id="banner-kegiatan" type="file" name="foto_banner_kegiatan" id="bannerKegiatan"
+									placeholder="Photo" alt="Pilih Foto" required="" capture>
 							</div>
 						</div>
 						<div class="form-group py-2">
 							<label for="namaKegiatan">Nama Kegiatan *</label>
-							<input type="text" class="form-control" id="nama-kegiatan" name="judul_kegiatan" placeholder="Contoh: Kegiatan Pelatihan" required>
+							<input type="text" class="form-control" id="nama-kegiatan" name="judul_kegiatan"
+								placeholder="Contoh: Kegiatan Pelatihan" required>
 						</div>
 						<div class="form-group py-2">
 							<label for="deskripsiKegiatan">Deskripsi Kegiatan *</label>
-							<textarea type="text" class="form-control" id="deskripsi-kegiatan" name="deskripsi_kegiatan" placeholder="Contoh: Ini adalah deskripsi kegiatan pelatihan" required></textarea>
+							<textarea type="text" class="form-control" id="deskripsi-kegiatan" name="deskripsi_kegiatan"
+								placeholder="Contoh: Ini adalah deskripsi kegiatan pelatihan" required></textarea>
 						</div>
 						<div class="row">
 							<div class="col">
 								<div class="form-group py-2">
 									<label for="tanggalMulaikegiatan">Tanggal Mulai *</label>
-									<input type="text" class="form-control js-daterangepicker" id="tanggal-mulai-kegiatan" data-drops="up" name="tanggal_kegiatan" value="" placeholder="Pilih tanggal kegiatan" required>
+									<input type="text" class="form-control js-daterangepicker"
+										id="tanggal-mulai-kegiatan" data-drops="up" name="tanggal_kegiatan" value=""
+										placeholder="Pilih tanggal kegiatan" required>
 									<small id="file-materi-kegiatan" class="form-text text-muted">
 										Tanggal mulai kegiatan
 									</small>
@@ -67,7 +74,9 @@
 							<div class="col">
 								<div class="form-group py-2">
 									<label for="tanggalSelesaikegiatan">Tanggal Selesai *</label>
-									<input type="text" class="form-control js-daterangepicker" id="tanggal-selesai-kegiatan" data-drops="up" name="tanggal_kegiatan_selesai" value="" placeholder="Pilih tanggal kegiatan" required>
+									<input type="text" class="form-control js-daterangepicker"
+										id="tanggal-selesai-kegiatan" data-drops="up" name="tanggal_kegiatan_selesai"
+										value="" placeholder="Pilih tanggal kegiatan" required>
 									<small id="file-materi-kegiatan" class="form-text text-muted">
 										Tanggal selesai kegiatan
 									</small>
@@ -76,7 +85,8 @@
 						</div>
 						<div class="form-group py-2">
 							<label for="provinsiKegiatan">Provinsi kegiatan *</label>
-							<select class="form-control" id="provinsi-kegiatan" name="id_provinsi" onChange="getState(this.value);" required>
+							<select class="form-control" id="provinsi-kegiatan" name="id_provinsi"
+								onChange="getState(this.value);" required>
 								<option selected disabled>Pilih Provinsi</option>
 								<?php foreach ($provinsi as $row) : ?>
 								<option value="<?= $row['id_provinsi']?>"><?= $row['provinsi']?></option>
@@ -91,16 +101,18 @@
 						</div>
 						<div class="form-group py-2">
 							<label for="lokasiKegiatan">Lokasi Kegiatan *</label>
-							<textarea type="text" class="form-control" id="lokasi-kegiatan" name="lokasi_kegiatan" placeholder="Contoh: Jalan demang lebar daun" required></textarea>
+							<textarea type="text" class="form-control" id="lokasi-kegiatan" name="lokasi_kegiatan"
+								placeholder="Contoh: Jalan demang lebar daun" required></textarea>
 						</div>
 						<div class="form-group py-2">
 							<label for="statusKegiatan">Status kegiatan *</label>
-							<select class="form-control" id="status-kegiatan" name="status_kegiatan" aria-placeholder="Pilih status kegiatan" required>
-							<option selected disabled>Pilih status kegiatan</option>
+							<select class="form-control" id="status-kegiatan" name="status_kegiatan"
+								aria-placeholder="Pilih status kegiatan" required>
+								<option selected disabled>Pilih status kegiatan</option>
 								<?php if($status_kegiatan != null):
 								foreach ($status_kegiatan as $row):?>
-									<option value="<?=$row['id_status_kegiatan']?>"><?= $row['status_kegiatan']?></option>
-							<?php endforeach; endif?>
+								<option value="<?=$row['id_status_kegiatan']?>"><?= $row['status_kegiatan']?></option>
+								<?php endforeach; endif?>
 							</select>
 						</div>
 						<div class="form-group py-2">
@@ -118,7 +130,8 @@
 						<div class="menu-divider"></div>
 						<button type="submit" class="btn btn-block btn-primary btn-modal-add-kegiatan">Tambah
 							Kegiatan</button>
-						<button type="button" class="btn btn-block btn-outline-dark btn-modal-close-add-kegiatan" data-dismiss="modal">Batal</button>
+						<button type="button" class="btn btn-block btn-outline-dark btn-modal-close-add-kegiatan"
+							data-dismiss="modal">Batal</button>
 					</form>
 				</div>
 			</div>
@@ -159,7 +172,8 @@
 											</div>
 											<div class="row no-gutters align-items-center">
 
-												<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?= $jumlah_seluruh_kegiatan ?> Kegiatan</div>
+												<div class="h5 mb-0 mr-3 text-gray-800">
+													<?= $jumlah_seluruh_kegiatan ?> Kegiatan</div>
 
 											</div>
 										</div>
@@ -178,7 +192,8 @@
 											</div>
 											<div class="row no-gutters align-items-center">
 
-												<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?= $jumlah_kegiatan_selesai ?> Kegiatan</div>
+												<div class="h5 mb-0 mr-3 text-gray-800">
+													<?= $jumlah_kegiatan_selesai ?> Kegiatan</div>
 
 											</div>
 										</div>
@@ -197,7 +212,8 @@
 											</div>
 											<div class="row no-gutters align-items-center">
 
-												<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?= $jumlah_kegiatan_berjalan ?> Kegiatan</div>
+												<div class="h5 mb-0 mr-3 text-gray-800">
+													<?= $jumlah_kegiatan_berjalan ?> Kegiatan</div>
 
 											</div>
 										</div>
@@ -208,36 +224,43 @@
 
 					</div>
 
-					<div class="container-fluid mt-4 container-background">
+					<div class="container-fluid mt-2 container-background">
 						<div class="col button-field">
-							<button class="btn btn-light btn-filter-kegiatan"><img class="img-profile mr-2" src="<?= base_url('assets/icons/pupr-filter-icon.svg') ?>">Filter</button>
-							<button class="btn btn-primary btn-add-kegiatan" data-toggle="modal" data-target="#modal-tambah-kegiatan"><img class="img-profile mr-2" src="<?= base_url('assets/icons/pupr-add-icon.svg') ?>">Add Kegiatan</button>
-							<button class="btn btn-outline-primary btn-import-kegiatan"><img class="img-profile mr-2" src="<?= base_url('assets/icons/pupr-import-icon.svg') ?>">Import Kegiatan</button>
+							<button class="btn btn-light btn-filter-kegiatan"><img class="img-profile mr-2"
+									src="<?= base_url('assets/icons/pupr-filter-icon.svg') ?>">Filter</button>
+							<button class="btn btn-primary btn-add-kegiatan" data-toggle="modal"
+								data-target="#modal-tambah-kegiatan"><img class="img-profile mr-2"
+									src="<?= base_url('assets/icons/pupr-add-icon.svg') ?>">Add Kegiatan</button>
+							<button class="btn btn-warning btn-import-kegiatan"><img class="img-profile mr-2"
+									src="<?= base_url('assets/icons/pupr-import-icon.svg') ?>">Import Kegiatan</button>
 						</div>
-						<table id="kalender_kegiatan" class="display">
-							<thead>
-								<tr>
-									<th>No</th>
-									<th>Tanggal Kegiatan</th>
-									<th>Banner Kegiatan</th>
-									<th>Jenis Kegiatan</th>
-									<th>Akun Kegiatan</th>
-									<th>Status Kegiatan</th>
-									<th>Nama Kegiatan</th>
-									<th>Banner Kegiatan</th>
-									<th>Instruktur Kegiatan</th>
-									<th>Assesor Kegiatan</th>
-									<th>Provinsi Kegiatan</th>
-									<th>Kota Kegiatan</th>
-									<th>Lokasi Kegiatan</th>
-									<th>Jumlah Peserta</th>
-									<th>Nama Materi Kegiatan</th>
-									<th>File Materi Kegiatan</th>
-									<th>Aksi</th>
-								</tr>
-							</thead>
-							
-						</table>
+						<div class="card-body">
+							<div class="table-responsive">
+								<table id="kalender_kegiatan_table" class="display">
+									<thead>
+										<tr>
+											<th>No</th>
+											<th>Tanggal Kegiatan</th>
+											<th>Banner Kegiatan</th>
+											<th>Jenis Kegiatan</th>
+											<th>Akun Kegiatan</th>
+											<th>Status Kegiatan</th>
+											<th>Nama Kegiatan</th>
+											<th>Instruktur Kegiatan</th>
+											<th>Assesor Kegiatan</th>
+											<th>Provinsi Kegiatan</th>
+											<th>Kota Kegiatan</th>
+											<th>Lokasi Kegiatan</th>
+											<th>Jumlah Peserta</th>
+											<th>Nama Materi Kegiatan</th>
+											<th>File Materi Kegiatan</th>
+											<th>Aksi</th>
+										</tr>
+									</thead>
+
+								</table>
+							</div>
+						</div>
 					</div>
 
 				</div>
