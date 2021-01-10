0d53c6ad09b57782cdb726e5357426fd0e8637a0
Mdedealf <dede.alfaruqq@gmail.com>
2021-01-10 20:49:45 +0700

    tambah illustrasi under construction, edit halaman dashboard

--- application/views/administrator/dashboard.php
+++ application/views/administrator/dashboard.php
@@ -32,21 +32,45 @@
 					<div class="row">
 
 						<div class="col-xl-3 col-md-6 mb-4">
-							<div class="card border-left-primary shadow h-100 py-2">
+							<div class="card h-100 p-2 card-recap">
 								<div class="card-body">
 									<div class="row no-gutters align-items-center">
 										<div class="col mr-2">
-											<div class="text-xl font-weight-bold text-black text-uppercase mb-1">
-												Total Peserta</div>
-										</div>
-										<div class="col-auto">
-											<i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+											<div class="h5 font-weight-bold text-black text-uppercase mb-2">
+												Total Kegiatan
+											</div>
+											<div class="row no-gutters align-items-center">
+												<div class="h5 mb-0 mr-3 text-gray-800">Kegiatan</div>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
 
+					</div>
+
+					<div class="row d-flex flex-column justify-content-center align-items-center">
+						<div class="col mt-4">
+							<div class="row d-flex flex-column justify-content-center align-items-center">
+								<div class="align-items-center">
+									<img class="img-profile mr-2" style="vertical-align:middle" src="<?= base_url('assets/illustration/pupr-illustration-under-construction.svg') ?>">
+								</div>
+							</div>
+						</div>
+						
+						<div class="col mt-4">
+							<div class="row d-flex flex-column justify-content-center align-items-center">
+								<div class="align-items-center">
+									<div>
+										<h3>Fitur ini akan segera hadir!</h3>
+									</div>
+									<div>
+										<p class="image-title">Menu Dashboard sedang dalam pengembangan, lihat kembali nanti!</p>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 
 				</div>
