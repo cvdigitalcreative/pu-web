d8d4514d1d447f3c274829cdf725871827359c85
zorayaw <zorayaw31@gmail.com>
2021-01-29 09:00:14 +0700

    get data lokasi edit kegiatan fixed

--- assets/js/administrator/administrator.js
+++ assets/js/administrator/administrator.js
@@ -2178,7 +2178,7 @@ Tidak ada poster kegiatan`					}
 			$('#banner-image-edit').attr('src', data['foto_banner_kegiatan'])
 			$('#edit-nama-kegiatan').val($(this).parent().siblings().eq(1).text())
 			$('#edit-deskripsi-kegiatan').val($(this).parent().siblings().eq(2).text())
-			$('#edit-lokasi-kegiatan').val($(this).parent().siblings().eq(12).text())
+			$('#edit-lokasi-kegiatan').val($(this).parent().siblings().eq(13).text())
 			$(`#modal-edit-kegiatan`).modal('show')
 		}
 		else if ($('#skkni_table').length > 0) {
