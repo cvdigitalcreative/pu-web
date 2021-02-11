e4bcbba7f89188d06dfaa7dc365ee06642ea4ff5
zorayaw <zorayaw31@gmail.com>
2021-02-11 19:54:01 +0700

    add button selesaikan kegiatan

--- application/views/administrator/events.php
+++ application/views/administrator/events.php
@@ -897,6 +897,30 @@
 	</div>
 	<!-- End of Lihat instruktur assesor Modal -->
 
+	<!-- Delete kegiatan modal -->
+	<div class="modal fade bd-example-modal-lg" id="modal-selesaikan-kegiatan" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-centered" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalCenterTitle">Ganti Status Kegiatan Ini?</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					Pastikan dengan benar bahwa Anda ingin menyelesaikan kegiatan ini!
+					<form>
+						<div class="modal-footer">
+							<button class="btn btn-light" data-dismiss="modal">Batal</button>
+							<button class="btn btn-selensaikan-kegiata" type="submit">Selesaikan Kegiatan</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- End modal delete kegiatan -->
+
 	<!-- Modal show image -->
 	<div id="show-image-modal" class="modal fade" role="dialog">
 		<div class="modal-dialog">
