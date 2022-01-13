73e1d00656e7051d9d56b7985a750e32e2f8ec96
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-01-13 15:50:55 +0700

    fix footer center, and add image home no scrollable

--- application/views/public.php
+++ application/views/public.php
@@ -56,7 +56,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
             <nav id="navbar" class="navbar">
                 <ul>
-                    <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+                    <li><a class="nav-link scrollto active" href="#hero1">Home</a></li>
                     <li class="dropdown"><a href="#about"><span>Mitra Balai</span> <i
                                 class="bi bi-chevron-down"></i></a>
                         <ul>
@@ -112,7 +112,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     </div>
                                 </div>
                                 <button type="button" class="btn btn-white">
-                                2020
+                                    2020
                                 </button>
                             </div>
                             <div class="btn-group">
@@ -131,7 +131,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     </div>
                                 </div>
                                 <button type="button" class="btn btn-white">
-                                2021
+                                    2021
                                 </button>
                             </div>
                             <div class="btn-group">
@@ -150,7 +150,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     </div>
                                 </div>
                                 <button type="button" class="btn btn-white">
-                                2022
+                                    2022
                                 </button>
                             </div>
                         </ul>
@@ -164,27 +164,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </header><!-- End Header -->
 
     <!-- ======= Hero Section ======= -->
-    <section id="hero" class="d-flex align-items-center">
-
-        <div class="container">
-            <div class="row">
-                <!-- <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
-                    <h1 data-aos="fade-up">Infografis</h1>
-                    <h2 data-aos="fade-up" data-aos-delay="400">Balai Jasa Konstruksi Wilayah II Palembang</h2>
-                    <div data-aos="fade-up" data-aos-delay="800">
-                        <a href="#contact" class="btn-get-started scrollto">Selengkapnya</a>
-                    </div>
-                </div> -->
-                <!-- <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
-                    <img src="<?=base_url('assets/image/pupr_logo.png')?>" class="img-fluid animated" alt="">
-                </div> -->
-            </div>
-        </div>
-
-    </section><!-- End Hero -->
+    <!-- End Hero -->
 
     <main id="main">
 
+        <div id="hero1">
+
+            <img id="home1" src="<?=base_url('assets/image/home1.png')?>" width="100%" height="100%" alt="">
+
+        </div>
         <!-- ======= Clients Section ======= -->
         <section id="clients" class="clients clients">
             <div class="container">
@@ -1793,8 +1781,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                             <h3>Balai Jasa Konstruksi Wilayah II Palembang</h3>
                             <p>Layanan Sosial Media Kami.</p>
                             <div class="social-links">
-                                <a href="Balai Jasa Konstruksi Wilayah II" class="facebook"><i class="bi bi-facebook"></i></a>
-                                <a href="https://instagram.com/pupr_bjkw2_palembang" class="instagram" target-blank><i class="bi bi-instagram"></i></a>
+                                <a href="Balai Jasa Konstruksi Wilayah II" class="facebook"><i
+                                        class="bi bi-facebook"></i></a>
+                                <a href="https://instagram.com/pupr_bjkw2_palembang" class="instagram" target-blank><i
+                                        class="bi bi-instagram"></i></a>
                             </div>
                         </div>
                     </div>
@@ -1803,7 +1793,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         <div class="info">
                             <div>
                                 <i class="ri-map-pin-line"></i>
-                                <p>Jl. Kasnariansyah, No. 78, KM 4.5, Kel. 20 Ilir, Kec. Ilir Timur I, <br>Kota Palembang, Sumatera Selatan, Indonesia</p>
+                                <p>Jl. Kasnariansyah, No. 78, KM 4.5, Kel. 20 Ilir, Kec. Ilir Timur I, <br>Kota
+                                    Palembang, Sumatera Selatan, Indonesia</p>
                             </div>
 
                             <!-- <div>
@@ -1835,7 +1826,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <footer id="footer">
         <div class="container">
             <div class="row d-flex align-items-center">
-                <div class="col-lg-6 text-lg-left text-center">
+                <div class="col text-lg-center text-center">
                     <div class="copyright">
                         &copy; Balai Jasa <strong>Konstruksi</strong> Wilayah II
                     </div>
@@ -1843,10 +1834,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         Palembang
                     </div>
                 </div>
-            
+
             </div>
         </div>
-    </footer><!-- End Footer -->
+    </footer>
+    <!-- End Footer -->
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
