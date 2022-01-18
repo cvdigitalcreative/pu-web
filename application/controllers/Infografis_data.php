99b63bd2438f39d936b64b0df4b87764c50cb9bd
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-01-18 19:14:00 +0700

    RPBWP CRUD Datatables

--- application/controllers/Infografis_data.php
+++ application/controllers/Infografis_data.php
@@ -1041,6 +1041,49 @@ class Infografis_data extends CI_Controller
         
     }
 
+    public function infografis_table_RPBWP()
+    {
+        if($this->session->userdata('logged_in') == true){
+            $data['infografis_table_RPBWP'] = $this->Infografis_model->data_table_chart_infografis($this->session->userdata('token'), $this->session->userdata('id_provinsi'), 14);
+            if($data['infografis_table_RPBWP'] == null){
+                $callback = array(
+                    'data' => []
+                );
+            }else{
+                if($data['infografis_table_RPBWP']['status'] == "Success"){
+                    if(count($data['infografis_table_RPBWP']['data']) > 0){
+                        $data['infografis_table_RPBWP'] = $data['infografis_table_RPBWP']['data'];
+                        $index_data_infografis = 0;
+                        $no_data_infografis = 1;
+                        foreach ($data['infografis_table_RPBWP'] as $val) {
+                            $data['infografis_table_RPBWP'][$index_data_infografis]['no_data_infografis'] = $no_data_infografis;
+
+                            $index_data_infografis++;
+                            $no_data_infografis++;
+                        }
+                        $callback = array(
+                            'data' => $data['infografis_table_RPBWP']
+                        );
+                    }else{
+                        $callback = array(
+                            'data' => []
+                        );
+                    }
+                }else{
+                    $callback = array(
+                        'data' => []
+                    );
+                }
+                header('Content-Type: application/json');
+                echo json_encode($callback);  
+            }
+        }else{
+            redirect('pupr/login');
+        }
+       
+        
+    }
+
     public function infografis_file_mitra(){
         if($this->session->userdata('logged_in') == true){
             $data['infografis_file_mitra_table'] = $this->Infografis_File_model->data_table_file_infografis($this->session->userdata('token'), $this->session->userdata('id_provinsi'), 1);
